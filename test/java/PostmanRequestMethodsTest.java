e88bfad0c8f0dc1041e05dc9bb64aa424ca26cfc
Maloi420 <150810281+Maloi420@users.noreply.github.com>
2024-11-23 14:39:12 +0100

    Add files via upload

---
+++ test/java/PostmanRequestMethodsTest.java
@@ -0,0 +1,114 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanRequestMethodsTest {
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://postman-echo.com";
+    }
+
+    @Test
+    public void testGet() {
+        given()
+                .queryParam("name", "Thomas")
+                .queryParam("occupation", "President")
+                .when()
+                .get("/get")
+                .then()
+                .statusCode(200)
+                .body("args.name", equalTo("Thomas"))
+                .body("args.occupation", equalTo("President"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.connection", equalTo("close"))
+                .body("url", equalTo("https://postman-echo.com/get?name=Thomas&occupation=President"));
+    }
+
+    @Test
+    public void testDelete() {
+        given()
+                .queryParam("item", "12345")
+                .when()
+                .delete("/delete")
+                .then()
+                .statusCode(200)
+                .body("args.item", equalTo("12345"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.connection", equalTo("close"))
+                .body("url", equalTo("https://postman-echo.com/delete?item=12345"));
+    }
+
+    @Test
+    public void testPatch() {
+        given()
+                .contentType("application/json")
+                .body("{\"title\": \"Emancipation Proclamation\"}")
+                .when()
+                .patch("/patch")
+                .then()
+                .statusCode(200)
+                .body("data.title", equalTo("Emancipation Proclamation"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.connection", equalTo("close"))
+                .body("url", equalTo("https://postman-echo.com/patch"));
+    }
+
+    @Test
+    public void testPostBody() {
+        given()
+                .body("{\"event\": \"Inauguration\", \"year\": 1861}")
+                .contentType("application/json")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data.event", equalTo("Inauguration"))
+                .body("data.year", equalTo(1861))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.connection", equalTo("close"))
+                .body("url", equalTo("https://postman-echo.com/post"));
+    }
+
+    @Test
+    public void testPostData() {
+        given()
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("occupation", "Commander-in-Chief")
+                .formParam("year", "1861")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("form.occupation", equalTo("Commander-in-Chief"))
+                .body("form.year", equalTo("1861"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.connection", equalTo("close"))
+                .body("url", equalTo("https://postman-echo.com/post"));
+    }
+
+    @Test
+    public void testPut() {
+        given()
+                .contentType("application/json")
+                .body("{\"role\": \"Leader\", \"term\": 4}")
+                .when()
+                .put("/put")
+                .then()
+                .statusCode(200)
+                .body("data.role", equalTo("Leader"))
+                .body("data.term", equalTo(4))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.connection", equalTo("close"))
+                .body("url", equalTo("https://postman-echo.com/put"));
+    }
+}
